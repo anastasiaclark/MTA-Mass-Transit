597aed9b51e2bc5b83290857934c986e3b964437
Anastasia Clark <anastasiapotupalova@gmail.com>
2024-06-04 11:08:17 -0400

    reformat inputs

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 
-
 from mta_gtfs_data_getter import download_gtfs_data
-from mta_gtfs_shapefiles_maker import make_bus_routes_shapefiles, make_bus_stops_shapefiles, make_rail_routes_shapefiles, make_rail_stops_shapefiles, make_subway_entrances_shapefiles
+from mta_gtfs_shapefiles_maker import (
+    make_bus_routes_shapefiles,
+    make_bus_stops_shapefiles,
+    make_rail_routes_shapefiles,
+    make_rail_stops_shapefiles,
+    make_subway_entrances_shapefiles,
+)
+
+from datetime import datetime
 
 path_name = os.getcwd()
 # folder = "July2019"
-folder = input(
-    'Type in the name of the folder (ex: Oct2016) where the original data for each MTA service is stored: ')
+folder = datetime.today().strftime("%b%Y")
 
 rails = ["LIRR", "metro_north", "nyc_subway"]
 
